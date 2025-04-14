0402a8c12e4e876c35b63fc8333a1dc681884aeb
htp24 <129657459+htp24@users.noreply.github.com>
2025-04-14 17:05:54 +0000

    Remove chemistry

--- chemistry/test.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE test (
-          id INTEGER NOT NULL,
-  student_id TEXT NOT NULL,
-       score NUMERIC NOT NULL,
-         day INTEGER NOT NULL,
-       month INTEGER NOT NULL,
-        year INTEGER NOT NULL
-);
